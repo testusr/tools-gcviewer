2d2839b96e7de1662e0c4f4ed99c69debe70cd84
Thorsten Ruehl <smeo@gmx.de>
2014-01-08 19:52:37 +0100

    added capability to also read relative time markers

--- src/main/java/com/smeo/tools/gc/domain/CurrentTimeTracker.java
+++ src/main/java/com/smeo/tools/gc/domain/CurrentTimeTracker.java
@@ -1,5 +1,7 @@
 package com.smeo.tools.gc.domain;
 
+import com.sun.corba.se.spi.activation._InitialNameServiceImplBase;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -7,24 +9,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by joachim on 25.12.13.
+ * can handle formatted time markers
+ *             2013-12-17T14:44:14.637+0100:
+ * and relative time markers
+ *             121.596:
+ * Responsible for extracting the current time of the passed log entry
  */
 public class CurrentTimeTracker {
     private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-    private static final Pattern timePattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\\.[0-9]{3}");
+    private static final Pattern formattedTimePattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\\.[0-9]{3}");
+    private static final Pattern relativeTimePattern = Pattern.compile("^[0-9]+\\.[0-9]+:");
+
+    Boolean basedOnFormattedTime = null;
 
     private long currTime = -1;
 
     public boolean extractLoggedTime(String line) throws IOException, ParseException {
-        Matcher timeMatcher = timePattern.matcher(line);
-        if (timeMatcher.find()){
-            currTime = simpleDateFormat.parse(timeMatcher.group()).getTime();
-            return true;
+        if (basedOnFormattedTime == null || basedOnFormattedTime) {
+            Matcher timeMatcher = formattedTimePattern.matcher(line);
+            if (timeMatcher.find()) {
+                currTime = simpleDateFormat.parse(timeMatcher.group()).getTime();
+                basedOnFormattedTime = true;
+                return true;
+            }
+        }
+
+        if (basedOnFormattedTime == null || !basedOnFormattedTime) {
+            Matcher relativeTimeMatcher = relativeTimePattern.matcher(line);
+            if (relativeTimeMatcher.find()){
+                basedOnFormattedTime = false;
+                String[] relativeTimeElements = relativeTimeMatcher.group().split("\\.|:");
+                currTime = Long.valueOf(relativeTimeElements[0]) * 1000;
+                currTime +=  Long.valueOf(relativeTimeElements[1]);
+                return true;
+            }
         }
         return false;
     }
 
-    public GcLoggedEvent updateGcEventTiming(GcLoggedEvent loggedEvent){
+    public Boolean isBasedOnFormattedTime() {
+        return basedOnFormattedTime;
+    }
+
+    public GcLoggedEvent updateGcEventTiming(GcLoggedEvent loggedEvent) {
         loggedEvent.setTimestamp(currTime);
         return loggedEvent;
     }
