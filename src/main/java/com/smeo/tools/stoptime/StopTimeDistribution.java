9ce14033f070d52f35cbeebb4a152f732042f107
testusr <smeo@gmx.de>
2015-11-22 09:27:03 +0200

    matching save points to relative time of application runtime entries, printing matching statistics

--- src/main/java/com/smeo/tools/stoptime/StopTimeDistribution.java
+++ src/main/java/com/smeo/tools/stoptime/StopTimeDistribution.java
@@ -10,49 +10,81 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Trying to correlate safe point data to application stop time logs with the goal to find out
- * which operations caused how much stop time.
+ * Trying to correlate safe point data to application stop stopTimeEntryDateTime logs with the goal to find out
+ * which operations caused how much stop stopTimeEntryDateTime.
  */
 public class StopTimeDistribution {
-    //    String toMatch = "2015-11-15T16:05:37.198+0000: 0,160: Total time for which application threads were stopped: 0,0000933 seconds, Stopping threads took: 0,0000211 seconds";
-    private final static String decimalRexep = "[0-9]+\\" + PatternFactory.decimalMarker() + "[0-9]+";
-    private final static String stopTimeRegexp = "([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\\.[0-9]{3}\\+[0-9]{4}): (" + decimalRexep + "): .*: (" + decimalRexep + ") seconds, .*";
+    //    String toMatch = "2015-11-15T16:05:37.198+0000: 0,160: Total stopTimeEntryDateTime for which application threads were stopped: 0,0000933 seconds, Stopping threads took: 0,0000211 seconds";
+    private final static String decimalRegexp = "[0-9]+\\" + PatternFactory.decimalMarker() + "[0-9]+";
+    private final static String gcDateTimeRelativeRegexp = "([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\\.[0-9]{3}\\+[0-9]{4}): (" + decimalRegexp + "):";
+    private final static String stopTimeRegexp = ""+gcDateTimeRelativeRegexp + " .*: (" + decimalRegexp + ") seconds, .*";
+    // 2015-11-22T07:48:26.438+0200: 2,857: Application stopTimeEntryDateTime: 0,1547989 seconds
+    private final static String runTimeRegexp = gcDateTimeRelativeRegexp + " Application time: (" + decimalRegexp + ") seconds.*";
+
     private final static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
-    private final static String safePointRegexp = "(" + decimalRexep + "): ([A-Za-z]+) +\\[.*\\] +[0-9]+ +";
+    private final static String safePointRegexp = "(" + decimalRegexp + "): ([A-Za-z]+) +\\[.*\\] +[0-9]+ +";
 
     private Pattern stopTimePattern = Pattern.compile(stopTimeRegexp);
+    private Pattern runTimePattern = Pattern.compile(runTimeRegexp);
     private Pattern safePointPatter = Pattern.compile(safePointRegexp);
 
-    private List<StopTimeEntry> stopTimeEntries = new ArrayList<StopTimeEntry>();
-    private double oldestRelativeStopTime  =  0.0;
-    private int reasonsAssigned = 0;
+    private List<StopRunTimeEntry> stopTimeEntries = new ArrayList<StopRunTimeEntry>();
+    private double oldestRelativeStopTime = 0.0;
+    private int safePointDataEntriesAssigned = 0;
+    private int safePointDataEntryCount = 0;
 
     private void loadStopTimeEntries(String filename) {
+        System.out.println(stopTimeRegexp);
+        System.out.println(runTimeRegexp);
+
         FileParser fileParser = new FileParser() {
+            StopRunTimeEntry currStopTimeEntry = null;
+
             @Override
             boolean parseLine(String line) {
-                Matcher matcher = stopTimePattern.matcher(line);
-                if (matcher.matches()) {
+                Matcher stopTimeMatcher = stopTimePattern.matcher(line);
+                if (stopTimeMatcher.matches()) {
                     try {
-                        long dateTime = simpleDateFormat.parse(getGroup(matcher, 1)).getTime();
-                        double relativeTime = PatternFactory.toDouble(getGroup(matcher, 2));
-                        double stopTime = PatternFactory.toDouble(getGroup(matcher, 3));
-
-                        addStopTimeEntry(dateTime, relativeTime, stopTime);
+                        if (currStopTimeEntry != null) {
+                            addStopTimeEntry(currStopTimeEntry);
+                        }
+                        long dateTime = simpleDateFormat.parse(getGroup(stopTimeMatcher, 1)).getTime();
+                        double relativeTime = PatternFactory.toDouble(getGroup(stopTimeMatcher, 2));
+                        double stopTime = PatternFactory.toDouble(getGroup(stopTimeMatcher, 3));
+                        this.currStopTimeEntry = new StopRunTimeEntry(dateTime, relativeTime, stopTime);
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
+                } else {
+                    Matcher runTimeMatcher = runTimePattern.matcher(line);
+                    if (runTimeMatcher.matches()) {
+                        long dateTime;
+                        try {
+                            dateTime = simpleDateFormat.parse(getGroup(runTimeMatcher, 1)).getTime();
+
+                            double relativeTime = PatternFactory.toDouble(getGroup(runTimeMatcher, 2));
+                            double runtime = PatternFactory.toDouble(getGroup(runTimeMatcher, 3));
+                            if (currStopTimeEntry != null) {
+                                currStopTimeEntry.addRuntimeInfo(dateTime, relativeTime, runtime);
+                            }
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
+
+
                 return true;
+
             }
         };
         fileParser.parseFile(filename);
     }
 
-    private void addStopTimeEntry(long dateTime, double relativeTime, double stopTime) {
-        stopTimeEntries.add(new StopTimeEntry(dateTime, relativeTime, stopTime));
-        oldestRelativeStopTime = relativeTime;
+    private void addStopTimeEntry(StopRunTimeEntry stopTimeEntry) {
+        stopTimeEntries.add(stopTimeEntry);
+        oldestRelativeStopTime = stopTimeEntry.runTimeEntryRelativeTime;
 
     }
 
@@ -61,11 +93,7 @@ public class StopTimeDistribution {
         return matchedLine.substring(matcher.start(groupIndex), matcher.end(groupIndex));
     }
 
-    private static String num(int digits, char postfix) {
-        return "[0-9]{" + digits + "}" + postfix;
-    }
-
-    private void enrichWithReason(List<StopTimeEntry> entriesToEnrich, String filename) {
+    private void enrichWithSafePointData(String filename) {
         FileParser fileParser = new FileParser() {
             @Override
             boolean parseLine(String line) {
@@ -73,14 +101,15 @@ public class StopTimeDistribution {
                 if (matcher.matches()) {
                     double relativeTime = PatternFactory.toDouble(getGroup(matcher, 1));
                     String reason = getGroup(matcher, 2);
+                    safePointDataEntryCount++;
 
-                    if (!youngerThanYoungestStopTimeEntry(relativeTime)){
-                    StopTimeEntry potentialMatch = findStopTimeEntryYoungerClosestTo(relativeTime);
-                    if (potentialMatch != null){
-                        reasonsAssigned++;
-                        potentialMatch.setReason(reason.intern());
-                    }
-                    return !olderThanOldestStopTimeEntry(relativeTime);
+                    if (!youngerThanYoungestStopTimeEntry(relativeTime)) {
+                        StopRunTimeEntry potentialMatch = findStopTimeEntryYoungerClosestTo(relativeTime);
+                        if (potentialMatch != null) {
+                            safePointDataEntriesAssigned++;
+                            potentialMatch.setSafePointData(relativeTime, reason.intern());
+                        }
+                        return true;// !olderThanOldestStopTimeEntry(relativeTime);
                     }
                 }
                 return true;
@@ -90,14 +119,14 @@ public class StopTimeDistribution {
     }
 
     private boolean youngerThanYoungestStopTimeEntry(double relativeTime) {
-        return relativeTime < stopTimeEntries.get(0).relativeTime;
+        return relativeTime < stopTimeEntries.get(0).runTimeEntryRelativeTime;
     }
 
-    private StopTimeEntry findStopTimeEntryYoungerClosestTo(double relativeTime) {
-        StopTimeEntry lastYoungerEntry = null;
-        for (int i=this.stopTimeEntries.size()-1; i > 0 ; i--){
-            StopTimeEntry currStopTimeEntry = stopTimeEntries.get(i);
-            if (currStopTimeEntry.youngerThanRelativeTime(relativeTime)){
+    private StopRunTimeEntry findStopTimeEntryYoungerClosestTo(double relativeTime) {
+        StopRunTimeEntry lastYoungerEntry = null;
+        for (int i = this.stopTimeEntries.size() - 1; i > 0; i--) {
+            StopRunTimeEntry currStopTimeEntry = stopTimeEntries.get(i);
+            if (currStopTimeEntry.youngerThanRelativeTime(relativeTime)) {
                 lastYoungerEntry = currStopTimeEntry;
             } else {
                 break;
@@ -108,46 +137,97 @@ public class StopTimeDistribution {
 
 
     private boolean olderThanOldestStopTimeEntry(double relativeTime) {
-        return relativeTime >= oldestRelativeStopTime ;
+        return relativeTime >= oldestRelativeStopTime;
     }
 
     public void run(String gcLogFilename, String stdoutFilename) {
         long start = System.currentTimeMillis();
         loadStopTimeEntries(gcLogFilename);
-        System.out.println("finished processing '"+stopTimeEntries.size()+"' stop time entries in " + ((System.currentTimeMillis() - start)/1000.0) + " seconds");
+        System.out.println("finished processing '" + stopTimeEntries.size() + "' stop stopTimeEntryDateTime entries in " + ((System.currentTimeMillis() - start) / 1000.0) + " seconds");
         if (this.stopTimeEntries == null) {
             System.out.println("stop times entries could not be created ... exiting");
             return;
         }
-        enrichWithReason(this.stopTimeEntries, stdoutFilename);
-        System.out.println("finished in " + ((System.currentTimeMillis() - start)/1000.0) + " seconds - " + reasonsAssigned + " reasons assigned");
+        enrichWithSafePointData(stdoutFilename);
+        System.out.println("finished in " + ((System.currentTimeMillis() - start) / 1000.0) + " seconds - " + safePointDataEntriesAssigned + " reasons assigned");
+
+        printConsistencyInformation();
+    }
+
+    private void printConsistencyInformation() {
+        int stopTimeEntries = this.stopTimeEntries.size();
+        System.out.println("## Printing consistency information for " + stopTimeEntries + "(rt)/"+ safePointDataEntryCount+"(sp) entries => " +(stopTimeEntries-safePointDataEntriesAssigned)+ " entries without safe point data");
+        int fullyConsistentEntries = 0;
+        int inconsistentEntries = 0;
+        int safePointInconsistencies = 0;
+        int runTimeInconsistencies = 0;
+        for (int i=0; i < this.stopTimeEntries.size(); i++){
+            StopRunTimeEntry currEntry = this.stopTimeEntries.get(i);
+            boolean runTimeStopTimeConsistent = currEntry.runTimeStopTimeConsistent();
+            boolean safePointConsistent = currEntry.safePointConsistent();
+            if (!runTimeStopTimeConsistent) runTimeInconsistencies++;
+            if (!safePointConsistent) safePointInconsistencies++;
+            if (!runTimeStopTimeConsistent || !safePointConsistent) {
+                inconsistentEntries++;
+            } else {
+                fullyConsistentEntries++;
+            }
+        }
+        System.out.println("fullyConsistentEntries: " + fullyConsistentEntries);
+        System.out.println("inconsistentEntries: " + inconsistentEntries);
+        System.out.println("safePointInconsistencies: " + safePointInconsistencies);
+        System.out.println("runTimeInconsistencies: " + runTimeInconsistencies);
+
     }
 
 
-    private static class StopTimeEntry {
-        private long time;
-        private double relativeTime;
+    private static class StopRunTimeEntry {
+        private long stopTimeEntryDateTime;
+        private double stopTimeEntryRelativeTime;
         private double stopTime;
+        private long runTimeEntryDateTime;
+        private double runTimeEntryRelativeTime;
+        private double runTime;
 
+        private double safePointRelativeTime;
         private String reason = null;
 
-        private StopTimeEntry(long dateTime, double relativeTime, double stopTime) {
-            this.time = dateTime;
-            this.relativeTime = relativeTime;
+        private StopRunTimeEntry(long dateTime, double relativeTime, double stopTime) {
+            this.stopTimeEntryDateTime = dateTime;
+            this.stopTimeEntryRelativeTime = relativeTime;
             this.stopTime = stopTime;
         }
 
-        public boolean youngerThanRelativeTime(double relativeTime) {
-            return this.relativeTime >= relativeTime;
+        public boolean runTimeStopTimeConsistent(){
+            if (!(runTimeEntryRelativeTime > 0 && stopTimeEntryRelativeTime > 0)) return false;
+            double expectedStopTimeEntryRelativeTime = runTimeEntryRelativeTime - runTime;
+            return (expectedStopTimeEntryRelativeTime - stopTimeEntryRelativeTime) < 1.0;
+        }
+        public boolean safePointConsistent(){
+            if (safePointRelativeTime <= 0){
+                return true;
+            }
+            return (Math.abs(runTimeEntryRelativeTime - safePointRelativeTime) < 10.0);
         }
 
-        public void setReason(String reason){
+        public boolean youngerThanRelativeTime(double relativeTime) {
+            return this.stopTimeEntryRelativeTime >= relativeTime;
+        }
+
+        public void setSafePointData(double relativeTime, String reason) {
             this.reason = reason;
+            this.safePointRelativeTime = relativeTime;
+        }
+
+        public void addRuntimeInfo(long dateTime, double relativeTime, double runTime) {
+            this.runTimeEntryDateTime = dateTime;
+            this.runTimeEntryRelativeTime = relativeTime;
+            this.runTime = runTime;
         }
 
         @Override
         public String toString() {
-            return reason + ", " + time + ", " + relativeTime + ", " + stopTime;
+            return reason + ", " + stopTimeEntryDateTime + ", " + stopTimeEntryRelativeTime + ", " + stopTime;
         }
     }
 
